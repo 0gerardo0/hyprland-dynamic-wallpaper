d329767b4012043318464859a325be52db563b82
Gerardo <therambien@hotmail.com>
2026-04-04 07:45:37 -0600

    Update script to use Big Sur Beach 2 and include full historical logic

--- wallpapers.sh
+++ wallpapers.sh
@@ -6,7 +6,7 @@
 # Compatible con la nueva sintaxis Hyprlang de hyprpaper v0.8+
 # Genera el config al vuelo y carga instantaneamente.
 
-DIR="$HOME/.local/share/backgrounds/desert-timed"
+DIR="$HOME/.local/share/backgrounds/big-sur-beach-timed"
 TMP_CONF="/tmp/hyprpaper_dynamic.conf"
 
 # Verificacion inicial
